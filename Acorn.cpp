d63cbeb8f16b9aefa7d0fa8bbdb27835414c00c6
Sea Grapes <natuloid@gmail.com>
2022-03-25 15:16:32 -0700

    working cli

--- Acorn.cpp
+++ Acorn.cpp
@@ -7,6 +7,8 @@ using namespace std;
 //#include <string>
 #include <vector>
 
+string VERSION = "1.0.0";
+
 struct Cmd {
   string path;            // path to sqrl executable
   vector<string> files;   // vector of acorn files to run
@@ -15,14 +17,21 @@ struct Cmd {
   bool cmdError = false;  // if error occurs, exit
 };
 
+
+void throwError(string err) {
+  cout << "Error: " << err << endl;
+}
+
 Cmd parseCmd(int argc, char* argv[]) {
 
   struct Cmd cmd;
   cmd.path = argv[0];
 
-  for (int i = 1; i < argc - 1; i++) {
+  for (int i = 1; i < argc; i++) {
 
     string arg = argv[i];
+
+    // flags
     if (arg.length() > 1 && arg[0] == '-') {
 
       string flag = arg.substr(1, string::npos);
@@ -37,22 +46,49 @@ Cmd parseCmd(int argc, char* argv[]) {
 
       else {
         throwError("unknown flag '" + flag + "'");
-        cout << "use command 'sqrl -h' or 'sqrl -help' for more information" << endl;
+        cmd.cmdError = true;
       }
 
     }
+    
+    // if not a flag, it must be a file to execute
+    else {
+      cmd.files.push_back(arg);
+    }
+
   }
   return cmd;
 }
 
-void throwError(string err) {
-  cout << "Error: " << err << endl;
-}
 
 int main(int argc, char* argv[])
 {
-  cout << argc << " arguments:" << endl;
-  for (int i = 0; i < argc; i++) {
-    cout << argv[i] << endl;
+
+  Cmd cmd = parseCmd(argc, argv);
+
+  //cout << cmd.path << endl;
+
+  if (cmd.cmdError) {
+    cout << "use command 'sqrl -h' or 'sqrl -help' for more information" << endl;
+    return 0;
   }
+
+  if (cmd.help) {
+    cout << "\nAcorn v" << VERSION << endl;
+    cout << "usage: sqrl [files] [options]\n" << endl;
+    cout << "options:" << endl;
+    cout << "  -v, -version    display Acorn version" << endl;
+    cout << "  -h, -help       list the help command" << endl;
+    return 0;
+  }
+
+  if (cmd.version) {
+    cout << "Acorn v" << VERSION << endl;
+    return 0;
+  }
+
+  //cout << argc << " arguments:" << endl;
+  //for (int i = 0; i < argc; i++) {
+  //  cout << argv[i] << endl;
+  //}
 }
